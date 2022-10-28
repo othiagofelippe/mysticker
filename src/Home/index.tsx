dd3069027e2e3d69e7a6749c9f143e7f19865b78
Thiago Felippe <thiagofelippe135@gmail.com>
2022-10-28 00:41:40 -0300

    open user camera

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,25 +1,44 @@
-import { useState } from 'react';
-import { Image, SafeAreaView, ScrollView, TextInput, View } from 'react-native';
+import { useState, useEffect } from "react";
+import { Image, SafeAreaView, ScrollView, TextInput, View } from "react-native";
+import { Camera, CameraType } from "expo-camera";
 
-import { Header } from '../components/Header';
-import { Button } from '../components/Button';
-import { PositionChoice } from '../components/PositionChoice';
+import { Header } from "../components/Header";
+import { Button } from "../components/Button";
+import { PositionChoice } from "../components/PositionChoice";
 
-import { styles } from './styles';
-import { POSITIONS, PositionProps } from '../utils/positions';
+import { styles } from "./styles";
+import { POSITIONS, PositionProps } from "../utils/positions";
 
 export function Home() {
-  const [positionSelected, setPositionSelected] = useState<PositionProps>(POSITIONS[0]);
+  const [hasCameraPermission, setHasCameraPermission] = useState(false);
+  const [positionSelected, setPositionSelected] = useState<PositionProps>(
+    POSITIONS[0]
+  );
+
+  useEffect(() => {
+    Camera.requestCameraPermissionsAsync().then((response) => {
+      setHasCameraPermission(response.granted);
+    });
+  }, []);
 
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView contentContainerStyle={styles.scroll} showsVerticalScrollIndicator={false}>
+      <ScrollView
+        contentContainerStyle={styles.scroll}
+        showsVerticalScrollIndicator={false}
+      >
         <View>
           <Header position={positionSelected} />
 
           <View style={styles.picture}>
-
-            <Image source={{ uri: 'https://github.com/rodrigorgtic.png' }} style={styles.camera} />
+            {hasCameraPermission ? (
+              <Camera style={styles.camera} type={CameraType.front} />
+            ) : (
+              <Image
+                source={{ uri: "https://github.com/rodrigorgtic.png" }}
+                style={styles.camera}
+              />
+            )}
 
             <View style={styles.player}>
               <TextInput
